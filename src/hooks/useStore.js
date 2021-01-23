5da0ae0d47311479c8b898c83e4124c9844879e4
michael-tanner-coder <michaelmontytanner@gmail.com>
2021-01-23 14:07:18 -0600

    Added hooks for intervals, keyboard controls, and data store

---
+++ src/hooks/useStore.js
@@ -0,0 +1,31 @@
+import create from 'zustand';
+
+const getLocalStorage = (key) => JSON.parse(window.localStorage.getItem(key));
+const setLocalStorage = (key, value) =>
+  window.localStorage.setItem(key, JSON.stringify(value));
+
+export const useStore = create((set) => ({
+  texture: 'dirt',
+  cubes: getLocalStorage('world') || [],
+  addCube: (x, y, z, texture) =>
+    set((state) => ({
+      cubes: [...state.cubes, { pos: [x, y, z], texture }],
+    })),
+  removeCube: (x, y, z) => {
+    set((state) => ({
+      cubes: state.cubes.filter((cube) => {
+        const [_x, _y, _z] = cube.pos;
+        return _x !== x || _y !== y || _z !== z;
+      }),
+    }));
+  },
+  setTexture: (texture) => {
+    set((state) => ({
+      texture,
+    }));
+  },
+  saveWorld: () =>
+    set((state) => {
+      setLocalStorage('world', state.cubes);
+    }),
+}));
